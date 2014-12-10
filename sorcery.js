190b45f007f3def4b040fdd50395ff49315c5f7f
levraijesus <nunezlopez.julian@gmail.com>
2014-12-10 02:58:23 +0100

    Menu ajout inventaire ° ajout histoire

--- sorcery.js
+++ sorcery.js
@@ -4,8 +4,15 @@ $(function() {
 
 	var inGame = false;
 	var life = "--";
+	var compagnon = null;
+	var inventaire = Array();
+
+	var hasReceivedParchemin = false;
+	var hasReceivedKey = false;
 
 	goToSectionJSON('acceuil');
+/*	executeAction('getParchemin');
+	executeAction('getClef');*/
 
 	//**Functions**\\
 	function initGame(){
@@ -24,23 +31,35 @@ $(function() {
 			//**INITIALISATION**\\
 			$('body > div').remove();
 			var promise=initPromise();
-			var myActionName =null;
 			var myPage=null;
 			//**RECUPERATION DES DONNEES**\\
 			promise.done(function(data){
 				$.each(data.pages,function(index, page){
+					/*SELECTION DE LA PAGE SOUHAITEE*/
 					if(myId == page.myId){
+						/*EXECUTION DE L'ACTION COURANTE*/
+						if(page.myAction != null){executeAction(page.myAction);}
+
 						myPage = "<div class='section' id='" + page.myId + "'>" ;
 					
-						if(page.myContentH2 != "null"){
+						if(page.myContentH2 != null){
 							myPage += "<h2>" + page.myContentH2 + "</h2>";}
 					
-						if(page.myContentTxt != "null"){
+						if(page.myContentTxt != null){
 							myPage += "<p>" + page.myContentTxt + "</p>";}
 						
-						if(page.myAction != "null"){
-							myPage += "<action name='" + page.myAction + "'/>";
-							myActionName=page.myAction;}
+						if(compagnon == 'Elf'){
+							if(page.myContentElf != null){
+								myPage += "<p class='contentCompagnon'>" + page.myContentElf + "</p>";}	
+						}
+
+						if(compagnon == 'Nain'){	
+							if(page.myContentNain != null){
+								myPage += "<p class='contentCompagnon'>" + page.myContentNain + "</p>";}
+						}
+
+						if(page.myAction != null){
+							myPage += "<action name='" + page.myAction + "'/>";}
 						
 						$.each(page.myGo,function(index, idGo){
 							myPage += "<button go='" + idGo+ "'>" + page.myGoContent[index] + "</button>";
@@ -49,29 +68,61 @@ $(function() {
 						myPage += "</div>";
 					}
 				});
-				/*EXECUTION DE L'ACTION COURANTE*/
-				if(myActionName!=null){executeAction(myActionName);}
-				
-				/*MISE EN FORME DIV STATUS*/
+				/*MISE EN FORME*/
 				if(myId!="acceuil" && myId!="contact"){
-				var divStatus = "<div id='status'><div class='life'>";
-				divStatus += "Life : <span class='value'>"+life+"</span>";
-				divStatus += "</div></div>";}
+					/*DIV STATUS*/	
+					var divStatus = "<div id='status'><div id='premierNiveau'><div class='life'>";
+					divStatus += "Life : <span class='value'>"+life+"</span></div>";
+					divStatus += "<div id='inventaire'>Inventaire</div></div></div>";
+
+					/*DIV INVENTAIRE*/
+					if(inventaire.length>0)
+					{
+						var divInventaireContent = "<div id='secondNiveau'><div id='inventaireContent'>";
+						for (var i=0; i<inventaire.length; i++){
+							if(inventaire[i].indexOf("Parchemin")!=-1){
+								divInventaireContent += "<span class='Parchemin'>"+inventaire[i]+"</span>";
+							}else if(inventaire[i].indexOf("Clef")!=-1){
+								divInventaireContent += "<span class='Clef'>"+inventaire[i]+"</span>";}
+						};
+						divInventaireContent += "</div></div>";
+					}else{
+						var divInventaireContent = "<div id='secondNiveau'><div id='inventaireContent'>";
+						divInventaireContent += "<span id='empty'>Inventaire vide</span>";
+						divInventaireContent += "</div></div>";
+					}
 				
+				}
 				//**AFFICHAGE DES DONNEES**\\
 				if(myPage!=null){
 					$('body').append(divStatus);
+					$('#status').append(divInventaireContent);
+					$('#inventaireContent').hide();	
 					$('body').append(myPage);
 					$('body > .section').hide();
 					$('body > .section').fadeIn(CONST_FADE_IN);}
 				
 				//**EVENEMENT**\\	
-				$(".section button").click( function() {
+				$('.section button').click( function() {
 					goToSectionJSON($(this).attr('go'));
 				});
-
+				$('#inventaire').click(function(){
+					$('#inventaireContent').slideDown(400);
+					$('#inventaire').hover(function() {
+					}, function() {
+						$('#inventaireContent').hover(function() {
+							$('#inventaireContent span').click(function(event) {
+								console.log($(this).html());
+							});
+						}, function() {
+							$('#inventaireContent').slideUp(300);
+							
+						});
+					});
+					//		$('#inventaireContent').slideDown(400);
+				});
 				//**VERIFICATION DE CONDITION DE FIN DE JEU**\\
-				if(life<=0 && inGame){endGame();}
+				if(life<=0 && inGame){inGame = false;}
 			});
 	}
 	
@@ -82,22 +133,32 @@ $(function() {
 				initGame();
 				break;
 			case 'start' :
-				startGame();
+				inGame = true;
 				break;
 			case 'hit' :
 				if(inGame){
 					life = life -1;
 				}
 				break;
+			case 'getNain' :
+				compagnon = 'Nain';
+				break;
+			case 'getElf' :
+				compagnon = 'Elf';
+				break;
+			case 'resetCompagnon' :
+				compagnon = null;
+				break;
+			case 'getParchemin' :
+				if(!hasReceivedParchemin){
+				inventaire.push("Parchemin de discrétion");
+				hasReceivedParchemin = true;}
+				break;
+			case 'getClef' :
+				if(!hasReceivedKey){
+				inventaire.push("Clef rouillée");
+				hasReceivedKey = true;}
+				break;
 		}
-	}
-
-	function startGame() {
-		inGame = true;
-	}
-	
-	function endGame() {
-		inGame = false;
-		goToSectionJSON('death');
 	}
 });
